fa747aa1ccdaa77cfb446d18ece2ed73b8ea2847
nasir-hafeez <kanwalnatasha83@gmail.com>
2023-07-29 08:29:22 -0700

    assignment5

---
+++ git/assignment4/module4script.js
@@ -0,0 +1,34 @@
+var names=new Array();
+names[0]="natasha";
+names[1]="hina";
+names[2]="kainat";
+names[3]="areej";
+names[4]="jam";
+names[5]="hammad";
+names[6]="jamal";
+
+
+    for(var i=0;i<names.length;i++){
+        if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+            console.log("Goodbye"+'  '+names[i]);}
+        else{
+        console.log("hello"+'  '+names[i]);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
